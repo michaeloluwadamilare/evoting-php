e5abf2162df41eecb35ca809e1075d57a20b8667
michaeloluwadamilare <darefelix95@gmail.com>
2021-03-27 03:10:05 +0100

    new heroku cleardb configuration

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6b16527573809eec8b2a6dc56fb51e450a562ab9',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6b16527573809eec8b2a6dc56fb51e450a562ab9',
+    ),
+  ),
+);
